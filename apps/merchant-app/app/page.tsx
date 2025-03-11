6724564ea61d5ad833019862babc2626b0936cdc
Ankit Mishra <aktmishra72@gmail.com>
2025-03-11 15:10:55 +0530

    setup

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,13 @@
+ 
+ 
+
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <h1 className="text-7xl text-red-700">
+        hello from merchant</h1> 
+
+       
+    </main>
+  );
+}
---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,14 @@
+ 
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+     
+
+       <h1 className="text-4xl font-extrabold text-red-800">hello from user app</h1>
+
+      
+
+      
+    </main>
+  );
+}
