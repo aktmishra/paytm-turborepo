02992b7d9c5b9e15984e73eba548ef6455f5c76e
Ankit Mishra <aktmishra72@gmail.com>
2025-03-12 08:16:46 +0530

    db setup complete

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,13 @@
- 
+
 export default function Page() {
+
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
      
 
        <h1 className="text-4xl font-extrabold text-red-800">hello from user app</h1>
 
-      
+      {/* <Card></Card> */}
 
       
     </main>
---
+++ apps/user-app/app/user/route.ts
@@ -0,0 +1,3 @@
+import { PrismaClient } from "@repo/db/client";
+
+const client = new PrismaClient()
